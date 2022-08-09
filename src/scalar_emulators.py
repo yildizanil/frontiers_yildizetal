0b069a3d209ef183c8cde9fd9f0dff96de48a668
Anil Yildiz <anilyildiz@tutanota.com>
2022-08-09 15:58:59 +0200

    scalar emulator class is defined

---
+++ src/scalar_emulators.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn import metrics
+from frontiers_yildizetal import Simulations
+
+import rpy2.robjects.packages as rpackages
+import rpy2.robjects.numpy2ri
+
+robustgasp = rpackages.importr('RobustGaSP')
+rpy2.robjects.numpy2ri.activate()
+
+class ScalarEmulators:
+    
+    def __init__(self, name:str, h_threshold, loc_x, loc_y):
+        self.name = name
+        filepath = 'tests/input/input_emulator_' + self.name + '.csv' 
+        self.input = pd.read_csv(filepath)
+        self.output = Simulations(self.name).curate_scalars(threshold=h_threshold, loc_x=loc_x, loc_y=loc_y)
+        
+    def emulate_scalar(self, scalar):
+        model = robustgasp.rgasp(design=self.input, response=self.output[scalar])
+        return (model)
