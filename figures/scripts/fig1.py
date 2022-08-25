24635763b05ac949277135b1fafbca4d668372b9
Anil Yildiz <anilyildiz@tutanota.com>
2022-08-25 17:07:09 +0200

    topo legend is added to fig1

--- figures/scripts/fig1.py
+++ figures/scripts/fig1.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import rasterio
 import numpy as np
 from pkg_resources import resource_filename
+import matplotlib as mpl
 
 simple = fy.Simulations('simple')
 
@@ -16,39 +17,47 @@ hfin = rasterio.open(simple.links['hfin'],'r').read(1)
 hmax_ma = np.ma.masked_where(hmax < 0.1, hmax, copy=True)
 hfin_ma = np.ma.masked_where(hfin < 0.1, hfin, copy=True)
 
-plt.subplot(121)
+fig, ((ax1,ax2)) = plt.subplots(nrows=1, ncols=2,sharey=True)
 
-plt.contourf(dem, extent=[0, 5000, 0, 4000],cmap='Greys',zorder=0)
-plt.vlines(x=3000,ymin=0,ymax=4000,zorder=0,color='k',linestyles='dotted')
-plt.imshow(hmax_ma, cmap='viridis', extent=[0, 5000, 0, 4000],zorder=1, vmin=0.1, vmax=20)
-plt.colorbar(location='top', orientation='horizontal', label='Maximum flow height [m]')
-plt.xlabel('Easting [m]')
-plt.ylabel('Northing [m]')
-plt.xticks(np.arange(0, 5001, 1000))
-plt.yticks(np.arange(0, 5001, 500))
-plt.xlim(0,5000)
-plt.ylim(1000,3000)
-plt.text(0, 4100, 'A', weight='bold')
-plt.text(3100, 1000,'Flat land at 0 m',ha='left',va='bottom')
+cmap = mpl.cm.Greys
+norm = mpl.colors.Normalize(vmin=0, vmax=1400)
 
-plt.subplot(122)
-plt.contourf(dem, extent=[0, 5000, 0, 4000],cmap='Greys',zorder=0)
-plt.vlines(x=3000,ymin=0,ymax=4000,zorder=0,color='k',linestyle='dotted')
-plt.imshow(hfin_ma, cmap='viridis', extent=[0, 5000, 0, 4000],zorder=1, vmin=0.1,vmax=4)
-plt.colorbar(location='top', orientation='horizontal', label='Deposit height [m]')
-plt.xlabel('Easting [m]')
-plt.xticks(np.arange(0, 5001, 1000))
-plt.yticks(np.arange(0, 5001, 500), labels=None)
-plt.xlim(0,5000)
-plt.ylim(1000,3000)
-ax2=plt.gca()
+c_topo = ax1.contourf(dem, extent=[0, 5000, 0, 4000],cmap='Greys',
+                      zorder=0,vmin=0,vmax=1400)
+ax1.vlines(x=3000,ymin=0,ymax=4000,zorder=0,color='k',linestyles='dotted')
+c1 = ax1.imshow(hmax_ma, cmap='viridis', extent=[0, 5000, 0, 4000],zorder=1, vmin=0.1, vmax=20)
+fig.colorbar(c1,ax=ax1,location='top', orientation='horizontal', label='Maximum flow height [m]')
+
+fig.subplots_adjust(bottom=0.5)
+cbar_ax = fig.add_axes([0.05,0,0.90,0.05])
+fig.colorbar(c_topo,cax=cbar_ax,cmap=cmap, norm=norm,
+             orientation='horizontal',label='Elevation [m]',shrink=0.36)
+
+ax1.set_xlabel('Easting [m]')
+ax1.set_ylabel('Northing [m]')
+ax1.set_xticks(np.arange(0, 5001, 1000))
+ax1.set_yticks(np.arange(0, 5001, 500))
+ax1.set_xlim(0,5000)
+ax1.set_ylim(1000,3000)
+ax1.text(0, 4100, 'A', weight='bold')
+ax1.text(3100, 1000,'Flat land at 0 m',ha='left',va='bottom')
+
+ax2.contourf(dem, extent=[0, 5000, 0, 4000],cmap='Greys',zorder=0,vmin=0,vmax=1400)
+ax2.vlines(x=3000,ymin=0,ymax=4000,zorder=0,color='k',linestyle='dotted')
+c2 = ax2.imshow(hfin_ma, cmap='viridis', extent=[0, 5000, 0, 4000],zorder=1, vmin=0.1,vmax=4)
+fig.colorbar(c2, ax=ax2,location='top', orientation='horizontal', label='Deposit height [m]')
+ax2.set_xlabel('Easting [m]')
+ax2.set_xticks(np.arange(0, 5001, 1000))
+ax2.set_yticks(np.arange(0, 5001, 500), labels=None)
+ax2.set_xlim(0,5000)
+ax2.set_ylim(1000,3000)
 ax2.axes.get_yaxis().set_ticklabels([])
-plt.text(0, 4100, 'B', weight='bold')
-plt.text(3100, 1000,'Flat land at 0 m',ha='left',va='bottom')
+ax2.text(0, 4100, 'B', weight='bold')
+ax2.text(3100, 1000,'Flat land at 0 m',ha='left',va='bottom')
 
 plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['font.size'] = 10
-plt.rcParams['figure.figsize'] = [18/2.54,8/2.54]
+plt.rcParams['figure.figsize'] = [18/2.54,7/2.54]
 plt.tight_layout()
 plt.subplots_adjust(wspace=0.1)
 
