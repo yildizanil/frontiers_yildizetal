1d3b1522b36593e3d5486ea7d4ca5784ca56dcb4
yildizanil <anilyildiz@tutanota.com>
2022-12-14 17:04:34 +0100

    code tidy up

--- figures/scripts/fig1.py
+++ figures/scripts/fig1.py
@@ -9,48 +9,69 @@ synth = fy.Simulations('synth')
 
 path = 'files/raster/elev.tif'
 dem_path = resource_filename('frontiers_yildizetal', path)
-dem = rasterio.open(dem_path, 'r').read(1)
 
-hmax = rasterio.open(synth.links['hmax'],'r').read(1)
-hfin = rasterio.open(synth.links['hfin'],'r').read(1)
-
-hmax_ma = np.ma.masked_where(hmax < 0.1, hmax, copy=True)
-hfin_ma = np.ma.masked_where(hfin < 0.1, hfin, copy=True)
-
-fig, ((ax1,ax2)) = plt.subplots(nrows=1, ncols=2)
+with rasterio.open(dem_path, 'r') as src:
+    dem = src.read(1)
+with rasterio.open(synth.links['hmax'],'r') as src:
+    hmax = src.read(1)
+    hmax_ma = np.ma.masked_where(hmax < 0.1, hmax, copy=True)
+with rasterio.open(synth.links['hfin'],'r') as src:
+    hfin = src.read(1)
+    hfin_ma = np.ma.masked_where(hfin < 0.1, hfin, copy=True)
 
 cmap = mpl.cm.Greys
 norm = mpl.colors.Normalize(vmin=0, vmax=1400)
 
-c_topo = ax1.contourf(dem, extent=[0, 5000, 0, 4000],cmap='Greys',
-                      zorder=0,vmin=0,vmax=1400)
-ax1.vlines(x=3000,ymin=0,ymax=4000,zorder=0,color='k',linestyles='dotted')
-c1 = ax1.imshow(hmax_ma, cmap='viridis', extent=[0, 5000, 0, 4000],zorder=1, vmin=0.1, vmax=20)
-fig.colorbar(c1,ax=ax1,location='top', orientation='horizontal', label='Maximum flow height [m]')
+fig, ((ax1,ax2)) = plt.subplots(nrows=1, ncols=2)
+
+c_topo = ax1.contourf(dem, extent=[0, 5000, 0, 4000],
+                      cmap='Greys', zorder=0,
+                      vmin=0, vmax=1400)
+ax1.vlines(x=3000, ymin=0, ymax=4000,
+           zorder=0, color='k',
+           linestyles='dotted')
+c1 = ax1.imshow(hmax_ma, cmap='viridis',
+                extent=[0, 5000, 0, 4000],
+                zorder=1, vmin=0.1, vmax=20)
+fig.colorbar(c1,ax=ax1,location='top',
+             orientation='horizontal',
+             label='Maximum flow height [m]')
 
 fig.subplots_adjust(bottom=0.5)
-cbar_ax = fig.add_axes([0.05,0,0.90,0.05])
-fig.colorbar(c_topo,cax=cbar_ax,cmap=cmap, norm=norm,
-             orientation='horizontal',label='Elevation [m]',shrink=0.36)
+cbar_ax = fig.add_axes([0.05, 0, 0.90, 0.05])
+fig.colorbar(c_topo, cax=cbar_ax, cmap=cmap, norm=norm,
+             orientation='horizontal',
+             label='Elevation [m]', shrink=0.36)
 
 ax1.set_xlabel('Easting [m]')
 ax1.set_ylabel('Northing [m]')
 ax1.set_xticks(np.arange(0, 5001, 1000))
 ax1.set_yticks(np.arange(0, 5001, 500))
-ax1.set_xlim(0,5000)
-ax1.set_ylim(1000,3000)
+ax1.set_xlim(0, 5000)
+ax1.set_ylim(1000, 3000)
 ax1.text(0, 3900, 'A', weight='bold')
-ax1.text(3100, 1000,'Flat land at 0 m',ha='left',va='bottom')
+ax1.text(3100, 1000,
+         'Flat land at 0 m',
+         ha='left', va='bottom')
 
-ax2.contourf(dem, extent=[0, 5000, 0, 4000],cmap='Greys',zorder=0,vmin=0,vmax=1400)
-ax2.vlines(x=3000,ymin=0,ymax=4000,zorder=0,color='k',linestyle='dotted')
-c2 = ax2.imshow(hfin_ma, cmap='viridis', extent=[0, 5000, 0, 4000],zorder=1, vmin=0.1,vmax=4)
-fig.colorbar(c2, ax=ax2,location='top', orientation='horizontal', label='Deposit height [m]')
+ax2.contourf(dem, extent=[0, 5000, 0, 4000],
+             cmap='Greys', zorder=0,
+             vmin=0, vmax=1400)
+ax2.vlines(x=3000, ymin=0, ymax=4000,
+           zorder=0, color='k',
+           linestyle='dotted')
+c2 = ax2.imshow(hfin_ma, cmap='viridis',
+                extent=[0, 5000, 0, 4000],
+                zorder=1, vmin=0.1, vmax=4)
+fig.colorbar(c2, ax=ax2, location='top',
+             orientation='horizontal',
+             label='Deposit height [m]')
+
 ax2.set_xlabel('Easting [m]')
 ax2.set_xticks(np.arange(0, 5001, 1000))
 ax2.set_yticks(np.arange(0, 5001, 500), labels=None)
-ax2.set_xlim(0,5000)
-ax2.set_ylim(1000,3000)
+ax2.set_xlim(0, 5000)
+ax2.set_ylim(1000, 3000)
 ax2.axes.get_yaxis().set_ticklabels([])
 ax2.text(0, 3900, 'B', weight='bold')
 ax2.text(3100, 1000,'Flat land at 0 m',ha='left',va='bottom')
--- figures/scripts/fig2.py
+++ figures/scripts/fig2.py
@@ -10,42 +10,45 @@ ac = fy.Simulations('acheron')
 
 path = 'files/raster/hillshade_acheron.tif'
 hill_path = resource_filename('frontiers_yildizetal', path)
+
 with rasterio.open(hill_path,'r') as hill:
     hill_arr = hill.read(1)
-hill_ma = np.ma.masked_where(hill_arr < -30000, hill_arr, copy=True)
-
+    hill_ma = np.ma.masked_where(hill_arr < -30000, hill_arr, copy=True)
 with rasterio.open(ac.links['hmax'],'r') as src:
     hmax = src.read(1)
+    hmax_ma = np.ma.masked_where(hmax < 0.1, hmax, copy=True)
 with rasterio.open(ac.links['hfin'],'r') as src2:
     hfin = src2.read(1)
-    
-hmax_ma = np.ma.masked_where(hmax < 0.1, hmax, copy=True)
-hfin_ma = np.ma.masked_where(hfin < 0.1, hfin, copy=True)
+    hfin_ma = np.ma.masked_where(hfin < 0.1, hfin, copy=True)
 
 fig, ((ax1,ax2)) = plt.subplots(nrows=1, ncols=2,
                                 gridspec_kw={'width_ratios': [1, 1]})
 
 ax1.imshow(hill_ma, cmap='Greys', extent=plotting_extent(hill))
 c1 = ax1.imshow(hmax_ma, cmap='viridis',
-                extent=plotting_extent(hill),zorder=1)
-fig.colorbar(c1,ax=ax1,location='top', orientation='horizontal',
-             label='Maximum flow height [m]',shrink=1)
+                extent=plotting_extent(hill), zorder=1)
+fig.colorbar(c1, ax=ax1, location='top',
+             orientation='horizontal',
+             label='Maximum flow height [m]', shrink=1)
+
 ax1.set_xlabel('Easting [x 10$^6$ m]')
 ax1.set_ylabel('Northing [x 10$^6$ m]')
-ax1.set_xticks(ticks=np.arange(1489000, 1493600,1000),
-               labels=[1.498,'1.490',1.491,1.492,1.493])
-ax1.set_yticks(ticks=np.arange(5201000, 5205001,1000),
-               labels=[5.201, 5.202,5.203,5.204,5.205])
+ax1.set_xticks(ticks=np.arange(1489000, 1493600, 1000),
+               labels=[1.498, '1.490', 1.491, 1.492, 1.493])
+ax1.set_yticks(ticks=np.arange(5201000, 5205001, 1000),
+               labels=[5.201, 5.202, 5.203, 5.204, 5.205])
 ax1.text(1488500, 5207000, 'A', weight='bold')
 
 ax2.imshow(hill_ma, cmap='Greys', extent=plotting_extent(hill))
 c2 = ax2.imshow(hfin_ma, cmap='viridis',
-                extent=plotting_extent(hill),zorder=1)
-fig.colorbar(c2, ax=ax2,location='top', orientation='horizontal',
-             label='Deposit height [m]',shrink=1)
+                extent=plotting_extent(hill), zorder=1)
+fig.colorbar(c2, ax=ax2,location='top',
+             orientation='horizontal',
+             label='Deposit height [m]', shrink=1)
+
 ax2.set_xlabel('Easting [x 10$^6$ m]')
-ax2.set_xticks(ticks=np.arange(1489000, 1493600,1000),
-               labels=[1.498,'1.490',1.491,1.492,1.493])
+ax2.set_xticks(ticks=np.arange(1489000, 1493600, 1000),
+               labels=[1.498, '1.490', 1.491, 1.492, 1.493])
 ax2.axes.get_yaxis().set_ticklabels([])
 ax2.text(1488500, 5207000, 'B', weight='bold')
 
@@ -53,7 +56,7 @@ plt.rcParams['font.family'] = 'Times New Roman'
 plt.rcParams['font.size'] = 10
 plt.rcParams['figure.figsize'] = [12/2.54,9/2.54]
 plt.tight_layout()
-plt.subplots_adjust(wspace=0.1,hspace=0.1)
+plt.subplots_adjust(wspace=0.1, hspace=0.1)
 
 plt.savefig('figures/PDF/fig2.pdf', format='pdf',
             bbox_inches='tight', facecolor='w')
